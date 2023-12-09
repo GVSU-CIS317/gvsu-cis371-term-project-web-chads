8e41ced02b18838b9efe8c40748b3caa161ef1d0
Timkie22 <70482394+Timkie22@users.noreply.github.com>
2023-12-09 13:23:38 -0500

    HTML/Markdown/BBCode added

--- webchads/src/components/Input.jsx
+++ webchads/src/components/Input.jsx
@@ -1,19 +1,22 @@
-import React, { useContext, useState } from "react";
-import { AuthContext } from "../context/AuthContext";
-import { ChatContext } from "../context/ChatContext";
+import React, { useContext, useState } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { ChatContext } from '../context/ChatContext';
 import {
   arrayUnion,
   doc,
   serverTimestamp,
   Timestamp,
   updateDoc,
-} from "firebase/firestore";
-import { db, storage } from "../firebase";
-import { v4 as uuid } from "uuid";
-import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+} from 'firebase/firestore';
+import { db, storage } from '../firebase';
+import { v4 as uuid } from 'uuid';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import ReactQuill from 'react-quill'; // Import ReactQuill
+import 'react-quill/dist/quill.snow.css'; // Import Quill styles
+import DOMPurify from 'dompurify'; // Import DOMPurify for sanitizing HTML
 
 const Input = () => {
-  const [text, setText] = useState("");
+  const [text, setText] = useState('');
   const [img, setImg] = useState(null);
 
   const { currentUser } = useContext(AuthContext);
@@ -27,14 +30,15 @@ const Input = () => {
 
       uploadTask.on(
         (error) => {
-          //TODO:Handle Error
+          // TODO: Handle Error
         },
         () => {
           getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-            await updateDoc(doc(db, "chats", data.chatId), {
+            const sanitizedHtml = DOMPurify.sanitize(text); // Sanitize the HTML content
+            await updateDoc(doc(db, 'chats', data.chatId), {
               messages: arrayUnion({
                 id: uuid(),
-                text,
+                text: sanitizedHtml, // Use sanitized HTML for text
                 senderId: currentUser.uid,
                 date: Timestamp.now(),
                 img: downloadURL,
@@ -44,41 +48,38 @@ const Input = () => {
         }
       );
     } else {
-      await updateDoc(doc(db, "chats", data.chatId), {
+      const sanitizedHtml = DOMPurify.sanitize(text); // Sanitize the HTML content
+      await updateDoc(doc(db, 'chats', data.chatId), {
         messages: arrayUnion({
           id: uuid(),
-          text,
+          text: sanitizedHtml, // Use sanitized HTML for text
           senderId: currentUser.uid,
           date: Timestamp.now(),
         }),
       });
     }
 
-    await updateDoc(doc(db, "userChats", currentUser.uid), {
-      [data.chatId + ".lastMessage"]: {
+    await updateDoc(doc(db, 'userChats', currentUser.uid), {
+      [data.chatId + '.lastMessage']: {
         text,
       },
-      [data.chatId + ".date"]: serverTimestamp(),
+      [data.chatId + '.date']: serverTimestamp(),
     });
 
-    await updateDoc(doc(db, "userChats", data.user.uid), {
-      [data.chatId + ".lastMessage"]: {
+    await updateDoc(doc(db, 'userChats', data.user.uid), {
+      [data.chatId + '.lastMessage']: {
         text,
       },
-      [data.chatId + ".date"]: serverTimestamp(),
+      [data.chatId + '.date']: serverTimestamp(),
     });
 
-    setText("");
-    setImg(null);
+    setText(''); // Reset text
+    setImg(null); // Reset image
   };
+
   return (
     <div className="input">
-      <input
-        type="text"
-        placeholder="Type something..."
-        onChange={(e) => setText(e.target.value)}
-        value={text}
-      />
+      <ReactQuill value={text} onChange={setText} />
       <div className="send">
         <button onClick={handleSend}>Send</button>
       </div>
